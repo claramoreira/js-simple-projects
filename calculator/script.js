2204e781464e47dab845d6864f0239461cd8af4a
Clara <clara_ljm@hotmail.com>
2022-08-22 19:51:43 -0300

    calculator logic

--- calculator/script.js
+++ calculator/script.js
@@ -3,21 +3,105 @@ class Calculator {
         this.buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
             '-', '+', 'x', '/', '=', 'C'];
         this.style = style;
+        this.visor = 0;
+        this.mem = null;
+        this.mem_op = null;
+        this.flag = 0;
     }
 
     showProps() {
         console.log(this.buttons);
         console.log(this.style);
-
         return
     }
 }
 
+document.addEventListener('click', function (e) {
+    let value;
+    if (e.target.classList.contains('button')) {
+        // this div has been clicked
+        clicked = e.target.id;
+        console.log(clicked)
+        button = parseInt(clicked)
+        if (!isNaN(button)) {
+            if (calc.visor == 0 || calc.flag == 1) {
+                calc.visor = button;
+                calc.flag = 0;
+            }
+            else {
+                calc.visor = calc.visor + '' + button;
+            }
+        }
+        else {
+            if (clicked === 'C') {
+                reset(calc);
+            }
+            else if (clicked === '=') {
+                if (calc.mem_op !== null && calc.mem !== null) {
+                    let operation = dict_function[calc.mem_op];
+                    calc.visor = operation(calc.mem, calc.visor);
+                    calc.mem_op = null;
+                }
+            }
+            else {
+                if (calc.mem_op === null) {
+                    value = 0;
+                    calc.mem = calc.visor;
+                }
+                else {
+                    let operation = dict_function[calc.mem_op];
+                    value = operation(calc.mem, calc.visor);
+                    calc.mem = value;
+                    calc.flag = 1;
+                }
+
+                calc.mem_op = clicked;
+                calc.visor = value;
+            }
+        }
+
+        console.log('Calc Mem: ' + calc.mem)
+        console.log('Calc Mem Op: ' + calc.mem_op)
+        console.log('Calc Visor: ' + calc.visor)
+        renderCalc(calc);
+    }
+});
+
+function add(a, b) {
+    return parseInt(a) + parseInt(b);
+}
+
+function subtract(a, b) {
+    return parseInt(a) - parseInt(b);
+}
+
+function divide(a, b) {
+    return parseInt(a) / parseInt(b);
+}
+
+function multiplicate(a, b) {
+    return parseInt(a) * parseInt(b);
+}
+
+function reset() {
+    calc.visor = 0;
+    calc.mem = null;
+    calc.mem_op = null;
+}
+
+const dict_function = {
+    '+': add,
+    '-': subtract,
+    '/': divide,
+    'x': multiplicate,
+    'C': reset
+}
+
 function renderCalc(calc) {
     let div = '', curr = '';
-    div = `<div class="header" id="header"></div>`
+    div = `<div class="header" id="header">${calc.visor}</div>`
     for (i = 0; i < calc.buttons.length; i++) {
-        curr = `<div class="button">${calc.buttons[i]}</div>`
+        curr = `<div class="button" id="${calc.buttons[i]}">${calc.buttons[i]}</div>`
         div = div + curr;
     }
     document.getElementById("buttons").innerHTML = div;
